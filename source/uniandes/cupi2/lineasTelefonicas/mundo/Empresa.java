8243bebc00d85c408777b18e59f658f60dfaee49
KevinPrada <santicastellanos2017@gmail.com>
2025-06-02 11:45:28 -0500

    Agregando llamada a celular a la línea telefónica 3

--- source/uniandes/cupi2/lineasTelefonicas/mundo/Empresa.java
+++ source/uniandes/cupi2/lineasTelefonicas/mundo/Empresa.java
@@ -224,6 +224,7 @@ public class Empresa
     public void agregarLlamadaCelularLinea3( int pMinutos )
     {
         // TODO Parte3 PuntoN: Completar el m�todo seg�n la documentaci�n dada.
+        linea3.agregarLlamadaCelular(pMinutos);
     }
 
     /**
