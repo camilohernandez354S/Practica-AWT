800d1fb36de70ec475f4d57478f958a2b3f26b71
KevinPrada <santicastellanos2017@gmail.com>
2025-06-02 11:36:22 -0500

    Reiniciando la línea telefónica, dejando valores en cero

--- source/uniandes/cupi2/lineasTelefonicas/mundo/LineaTelefonica.java
+++ source/uniandes/cupi2/lineasTelefonicas/mundo/LineaTelefonica.java
@@ -89,6 +89,9 @@ public class LineaTelefonica
 	public void reiniciar( )
 	{
         // TODO Parte2 PuntoE: Reiniciar el n�mero de llamadas, n�mero de minutos y costo de llamadas en 0.
+        numeroLlamadas = 0;
+        numeroMinutos = 0;
+        costoLlamadas = 0.0;
 	}
 
 	/**
